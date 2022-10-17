714aa20915aa959607bce48c02a5ba114c86df0f
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2022-10-17 18:37:31 -0300

    Add files via upload

--- CLASSES/sec3/paragrafos/assets/js/main.js
+++ CLASSES/sec3/paragrafos/assets/js/main.js
@@ -1,1 +1,12 @@
+const div = document.querySelector(".paragrafos");
+const ps = div.querySelectorAll("p");
 
+// selecciona todo lo relativo al estilo del body
+const estilosBody = getComputedStyle(document.body);
+const backgroundBody = estilosBody.backgroundColor;
+console.log(backgroundBody);
+
+for (p of ps) {
+  p.style.backgroundColor = backgroundBody;
+  console.log(p);
+}
