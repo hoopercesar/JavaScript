7bf7a1a4fed3ccbe6b8b80de45c07cb0cf48b4bc
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2022-10-23 21:08:23 -0300

    Add files via upload

--- CLASSES/sec3/lista_tareas/assets/js/main.js
+++ CLASSES/sec3/lista_tareas/assets/js/main.js
@@ -1,1 +1,18 @@
+const inputTarefa = document.querySelector(".input-tarefa");
+const btnTarefa = document.querySelector(".btn-tarefa");
+const tarefas = document.querySelector(".tarefas");
 
+function criaLi() {
+  return document.createElement("li");
+}
+
+function criaTarefa(argInput) {
+  const li = criaLi();
+  li.innerHTML = argInput;
+  tarefas.appendChild(li);
+}
+
+btnTarefa.addEventListener("click", function (ev) {
+  if (!inputTarefa.value) return alert("Ingrese tarea");
+  criaTarefa(inputTarefa.value);
+});
