6a51aee0314ba07eb11ee3423164dbc94a3b09b8
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2022-10-24 23:25:31 -0300

    Add files via upload

--- CLASSES/sec4/funciones4.js
+++ CLASSES/sec4/funciones4.js
@@ -9,3 +9,10 @@ function conta(operador, acumulador, ...numeros) {
   console.log(acumulador);
 }
 conta("-", 20, 3, 4, 5, 6);
+
+// cuando se necesita enviar una cantidad desconocida de argumentos
+// dentro de una función, se puede usar como parámetro la ...rest
+function params(...args) {
+  console.log(args);
+}
+params("-", 3, 4, 3, 7, 8, 12);
