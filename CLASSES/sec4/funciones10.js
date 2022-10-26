aa0d76fe64635ff74895145e6a86144a983806a2
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2022-10-26 19:22:44 -0300

    Add files via upload

--- CLASSES/sec4/funciones10.js
+++ CLASSES/sec4/funciones10.js
@@ -1,13 +1,43 @@
-// this
+// factory function
 
-function criaPessoa(nome, sobrenome) {
+function criaPessoa(nome, sobrenome, a, p) {
   return {
     nome,
     sobrenome,
-    fala: function (assunto) {
-      return `${nome} está falando do ${assunto}`;
+    get nomeCompleto() {
+      return `${this.nome} ${this.sobrenome}`;
+    },
+    // setter
+    set nomeCompleto(valor) {
+      // con set podemos introducir cambios a las variables
+      // en este caso, vamos a recoger el nombre que viene con valor
+      // y lo dejaremos como nome usando this.nome
+      // ahora, nomeCompleto tendrá el nombre que ingresamos
+      // con p1.nomeCompleto y el apellido que ingresamos al ejecutar
+      // la función criaPessoa
+      valor = valor.split(" "); // separa los elementos del nombre y forma array
+      this.nome = valor.shift(); // guarda nombre y lo elimina del array
+      this.sobrenome = valor.join(" "); // junta los apellidos en un solo string
+    },
+
+    fala(assunto) {
+      return `${this.nome} está falando do ${assunto}`;
+    },
+    altura: a,
+    peso: p,
+    // Getter
+    get imc() {
+      // con get, imc deja de ser un método y se trasnforma en
+      // en atributo, igual que nome, sobrenome
+      const indice = this.peso / this.altura ** 2;
+      return indice.toFixed(2);
     },
   };
 }
-const p1 = criaPessoa("cesar", "soto");
-console.log(p1);
+const p1 = criaPessoa("cesar", "soto", 1.8, 78);
+// const p2 = criaPessoa("maria", "peres", 1.6, 65);
+// console.log(p2.fala("está falando sobre JS"));
+p1.nomeCompleto = "fernanda sanchez hooper";
+console.log(p1.nomeCompleto);
+console.log(p1.nome);
+console.log(p1.sobrenome);
