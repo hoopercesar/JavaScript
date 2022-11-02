30779d5db7db021d0e13418c20a7b597cbf4c720
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2022-11-02 08:54:53 -0300

    Add files via upload

--- CLASSES/sec4/calculadora/assets/js/main2.js
+++ CLASSES/sec4/calculadora/assets/js/main2.js
@@ -6,10 +6,20 @@ function Calculadora() {
     this.clicqueBotoes();
   };
 
+  // función para mostrar numeros clicados
+  this.btnParaDisplay = function (valor) {
+    this.display.value += valor;
+  };
+
+  // función principal que llama otras funciones con el evento click
   this.clicqueBotoes = function () {
     document.addEventListener("click", (ev) => {
       const elem = ev.target;
       console.log(elem);
+
+      if (elem.classList.contains("btn-num")) {
+        this.btnParaDisplay(elem.innerText);
+      }
     });
   };
 }
