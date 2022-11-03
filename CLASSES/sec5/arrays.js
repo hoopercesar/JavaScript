b9489893508c25d0ba3bb4c801220c72860bc3fe
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2022-11-03 18:53:31 -0300

    Add files via upload

--- CLASSES/sec5/arrays.js
+++ CLASSES/sec5/arrays.js
@@ -1,4 +1,19 @@
 const nomes = ["laura", "iris", "luiz"];
 nomes[3] = "joao";
 delete nomes[3];
-console.log(nomes);
+
+// pop() y shift remueven el ultimo y el primer valor de un arreglo
+// ambos guardan el valor eliminado en una variable
+const final = nomes.pop();
+const inicial = nomes.shift();
+
+nomes.push("joana"); // agrega ultima posición
+nomes.unshift("carlos"); // agrega en posición 0
+nomes.unshift("petro");
+const novo = nomes.slice(0, -2);
+console.log(nomes, novo);
+
+const frase = "anatema va a ser una hermosa desafortunada";
+const arreglo = frase.split(" "); // separa por espacio
+console.log(arreglo);
+console.log(nomes.join(" "));
