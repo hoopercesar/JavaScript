b08c572032a86a3e260013385aba860106c2d1ac
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2022-11-05 08:42:08 -0300

    Add files via upload

--- CLASSES/sec5/splice.js
+++ CLASSES/sec5/splice.js
@@ -1,3 +1,14 @@
 const nomes = ["pepe", "kata", "jose", "fercho", "daniel", "joao"];
 
-console.log(nomes, nomes.splice(3, 3, "raja"));
+// array.splice(indice, delete, elem1, elem2, elem3, ...)
+// delete: cantidad de elemntos a borrar a partir del índice
+// elem1, elem2... son los elementos a adicionar
+// splice guarda los elemntos eliminados
+
+console.log(nomes, nomes.splice(2, 3)); // ['pepe', 'kata', 'joao']
+// eliminó 3 elementos, a partir del índice 2
+
+const elim = nomes.splice(1, 1);
+console.log(elim); // kata
+const agreg = nomes.splice(1, 0, "juan", "camila", "fer", "doroty");
+console.log(nomes); // [pepe, juan, camila, fer, doroty, joao]
