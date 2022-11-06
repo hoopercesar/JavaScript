284bbc6f09ebdb1213179c373de4559bafe6f886
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2022-11-05 23:52:37 -0300

    Add files via upload

--- CLASSES/sec5/filter.js
+++ CLASSES/sec5/filter.js
@@ -1,3 +1,5 @@
+// filter retorna un array con la cantiad original o menos
+
 const numeros = [5, 50, 1, 80, 2, 3, 4, 19, 27, 11, 15];
 // const algo = [];
 // for (let k of numeros) {
@@ -18,14 +20,35 @@ function callBackFilter(valor, indice, array) {
 const numerosFiltrados = numeros.filter((num) => num > 10);
 console.log(numerosFiltrados);
 
-const num2 = numeros.filter((num, indice) => {
-  console.log(num, indice);
-  return num > 10;
+const num2 = numeros.filter((num, indice, array) => {
+  // console.log(num, indice, array);
+  if (num > 10) {
+    return { num, indice, array };
+  }
+  // return num > 10;
 });
 console.log(num2);
+console.log("=================================");
 
-const nomes = ["pepe", "kata", "jose", "fercho", "daniel", "joao"];
+const pessoas = [
+  { nome: "pepe", edad: 34 },
+  { nome: "kata", edad: 13 },
+  { nome: "jose", edad: 37 },
+  { nome: "fercho", edad: 21 },
+  { nome: "daniel", edad: 93 },
+  { nome: "joao", edad: 8 },
+];
+// retornar personas con nombres con más de 4 letras
+const adultos = pessoas.filter((obj) => obj.nome.length > 4);
+console.log(adultos);
 
-const names = nomes.filter((nome) => nome[1] === "e");
-console.log(nomes);
+// retornar mayores de 40 años
+const viejos = pessoas.filter((obj) => obj.edad > 40);
+console.log(viejos);
+
+// retorna nombres que terminan con a
+const a = pessoas.filter((obj) => obj.nome.endsWith("a"));
+console.log(a[0]);
+
+// console.log(pessoas);
 // console.log(names);
