2b3d796602125b14e95f13a4d89bf48611264052
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2022-11-26 08:57:46 -0300

    Add files via upload

--- CLASSES/sec6/polimorfismo.js
+++ CLASSES/sec6/polimorfismo.js
@@ -21,19 +21,23 @@ function Conta(agencia, conta, saldo) {
   this.saldo = saldo;
 }
 
+// DEPOSITAR
 Conta.prototype.depositar = function (valor) {
   this.saldo += valor;
   this.verSaldo();
   this.verFecha();
 };
 
+// VER SALDO
 Conta.prototype.verSaldo = function () {
+  this.verFecha();
   console.log(
     `Ag/c: ${this.agencia}/${this.conta} | ` +
-      `Saldo: R${this.saldo.toFixed(2)}`
+      `Saldo: R$${this.saldo.toFixed(2)}`
   );
 };
 
+// lA FECHA
 Conta.prototype.verFecha = function () {
   const fecha = new Date();
   let indiceMes = fecha.getMonth();
@@ -43,10 +47,12 @@ Conta.prototype.verFecha = function () {
   );
 };
 
+// RETIRAR
 Conta.prototype.retirar = function (valor) {
   if (valor > this.saldo) {
     this.verFecha();
-    console.log(`Saldo Insuficiente. Saldo R$${this.saldo}`);
+    console.log(`Saldo Insuficiente`);
+    this.verSaldo();
     return;
   }
   this.saldo -= valor;
@@ -54,8 +60,47 @@ Conta.prototype.retirar = function (valor) {
   this.verSaldo();
 };
 
-Conta.prototype.da;
+// CUENTA CORRIENTE CC
+function CC(agencia, conta, saldo, limite) {
+  Conta.call(this, agencia, conta, saldo);
+  this.limite = limite;
+}
 
-const cuenta = new Conta(3922, 22, 30);
-cuenta.retirar(50);
+CC.prototype = Object.create(Conta.prototype);
+CC.prototype.constructor = CC;
+
+// RETIRAR de CC
+CC.prototype.retirar = function (valor) {
+  if (valor > this.saldo + this.limite) {
+    this.verFecha();
+    console.log(
+      `Saldo Insuficiente. Saque Máximo: R$${this.saldo + this.limite}`
+    );
+    // this.verSaldo();
+    return;
+  }
+  this.saldo -= valor;
+  this.verFecha();
+  this.verSaldo();
+};
+
+// CONTA POUPANÇA CP
+function CP(agencia, conta, saldo) {
+  Conta.call(this, agencia, conta, saldo);
+}
+
+CP.prototype = Object.create(Conta.prototype);
+CP.prototype.constructor = CP;
+
+const cp = new CP(11, 22, 10);
+cp.retirar(20);
+
+console.log();
+
+// const cuenta = new Conta(3922, 22, 30);
+// cuenta.retirar(21);
 // cuenta.verSaldo();
+
+const cc = new CC(11, 22, 0, 100);
+// cc.verSaldo();
+cc.retirar(120);
