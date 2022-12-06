abeb4b6d9d8e82edc0bfed5c83faeb67560079a1
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2022-12-06 09:34:57 -0300

    Add files via upload

--- CLASSES/sec7_class/criaClasses.js
+++ CLASSES/sec7_class/criaClasses.js
@@ -16,6 +16,18 @@ class Pessoa {
   }
 }
 
+// sólo para comparar class con la función constructora
+function Pessoa2(nome, sobrenome) {
+  this.nome = nome;
+  this.sobrenome = sobrenome;
+}
+
+Pessoa2.prototype.fala = function () {
+  console.log(`${this.nome} está falando`);
+};
+
 const p1 = new Pessoa("pepe", "soto");
+const p2 = new Pessoa2("jõao", "pires");
 // p1.falar();
 console.log(p1);
+console.log(p2);
