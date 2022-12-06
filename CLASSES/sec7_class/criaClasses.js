110ef9feca842f57ba62140df40df3d5b8b136d3
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2022-12-06 08:32:16 -0300

    Add files via upload

---
+++ CLASSES/sec7_class/criaClasses.js
@@ -0,0 +1,21 @@
+class Pessoa {
+  constructor(nome, sobrenome) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+  }
+  falar() {
+    console.log(`${this.nome} está falando`);
+  }
+
+  comer() {
+    console.log(`${this.nome} está comendo`);
+  }
+
+  beber() {
+    console.log(`${this.nome} está bebendo`);
+  }
+}
+
+const p1 = new Pessoa("pepe", "soto");
+// p1.falar();
+console.log(p1);
