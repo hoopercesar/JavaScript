7746c00ef0a63b150798946d01dd33a864a1dcaa
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2022-12-07 21:40:13 -0300

    Add files via upload

--- CLASSES/sec7_class/heranca.js
+++ CLASSES/sec7_class/heranca.js
@@ -22,9 +22,46 @@ class DispositivoEletronico {
   }
 }
 
-const d1 = new DispositivoEletronico("smartphone");
+class Smartphone extends DispositivoEletronico {
+  constructor(nome, cor, modelo) {
+    super(nome);
+    this.cor = cor;
+    this.modelo = modelo;
+  }
+}
 
-d1.ligar();
-console.log(d1);
-d1.desligar();
-console.log(d1);
+class Tablet extends Smartphone {
+  constructor(nome, cor, modelo, temWifi) {
+    super(nome, cor, modelo);
+    this.temWifi = temWifi;
+  }
+
+  ligar() {
+    console.log("você alterou a maneira de ligar");
+  }
+
+  falaOi() {
+    console.log("Oi, tudo bem?");
+  }
+}
+
+// const d1 = new DispositivoEletronico("smartphone");
+
+// d1.ligar();
+// console.log(d1);
+// d1.desligar();
+// console.log(d1);
+
+const s1 = new Smartphone("Aipon", "pink", "10");
+// s1.ligar();
+// console.log(s1);
+// s1.ligar();
+// console.log(s1);
+// s1.desligar();
+// console.log(s1);
+// s1.desligar();
+
+const t1 = new Tablet("XPP", "green", "USX9", "wifi");
+console.log(t1);
+t1.ligar();
+t1.falaOi();
