8307de9a5ec0e7f75b9bafb420790186e31819a4
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2022-12-16 21:18:26 -0300

    Add files via upload

--- CLASSES/sec7_class/validaFormulario/modelo/assets/js/main.js
+++ CLASSES/sec7_class/validaFormulario/modelo/assets/js/main.js
@@ -1,1 +1,44 @@
+class ValidaFormulario {
+  constructor() {
+    this.formulario = document.querySelector(".formulario");
+    this.eventos();
+  }
 
+  eventos() {
+    this.formulario.addEventListener("submit", (ev) => {
+      this.handleSubmit(ev);
+    });
+  }
+
+  // el evento que envía los datos por submit
+  handleSubmit(ev) {
+    ev.preventDefault();
+    const validos = this.saoCamposValidos();
+    console.log(validos);
+  }
+
+  saoCamposValidos() {
+    let valid = true;
+    document.querySelectorAll(".validar").forEach((campo) => {
+      if (!campo.value) {
+        this.criaErro(campo, "Debe llenar el campo ");
+      }
+      console.log(campo.classList[0]);
+    });
+
+    // for (let campo of this.formulario.querySelectorAll(".validar")) {
+    //   console.log(campo);
+    // }
+  }
+
+  criaErro(campo, mensaje) {
+    const div = document.createElement("div");
+    div.innerHTML = mensaje;
+    div.style.color = "red";
+    div.style.fontSize = "15px";
+    div.classList.add("error-text");
+    campo.insertAdjacentElement("afterend", div);
+  }
+}
+
+const valida = new ValidaFormulario();
