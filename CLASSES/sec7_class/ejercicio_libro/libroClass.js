1c333303b89d28cabb6ec7d6f021e4bba3a9ac85
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2022-12-26 09:36:16 -0300

    Add files via upload

--- CLASSES/sec7_class/ejercicio_libro/libroClass.js
+++ CLASSES/sec7_class/ejercicio_libro/libroClass.js
@@ -1,8 +1,42 @@
 class Libros {
-  constructor(genero, year, titulo, autor) {
-    this.genero = genero;
-    this.year = year;
-    this.titulo = titulo;
-    this.autor = autor;
+  constructor() {
+    this.formulario = document.querySelector(".formulario");
+    this.eventos();
+  }
+
+  eventos() {
+    this.formulario.addEventListener("submit", (ev) => {
+      this.handleSubmit(ev);
+    });
+  }
+
+  handleSubmit(ev) {
+    ev.preventDefault();
+    console.log(ev);
+    const validos = sonCamposValidos();
+  }
+
+  sonCamposValidos() {
+    let valid = true;
+
+    for (let textoError of this.formulario.querySelector(".error_text")) {
+      textoError.remove();
+    }
+
+    document.querySelectorAll(".validar").forEach((campo) => {
+      if (!campo.value) {
+        this.mensajeError(campo, "Este campo debe llenarse");
+        valid = false;
+      }
+    });
+  }
+
+  mensajeError(campo, mensaje) {
+    let div = document.createElement("div");
+    div.innerHTML = mensaje;
+    div.style.color = "red";
+    div.style.fontSize = "15px";
+    div.classList.add("error_text");
+    campo.insertAdjacentElement("afterend", div);
   }
 }
