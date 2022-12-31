3959c2cd3cceab786cf299f9bf8ab76be1234ef3
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2022-12-31 09:09:49 -0300

    Add files via upload

---
+++ CLASSES/sec7_class/ejercicio_libro/buscaLibros.js
@@ -0,0 +1,7 @@
+class BuscaLibros {
+  // script para buscar libros en un registro con libros
+  cosntructor() {
+    this.formulario = document.querySelector(".formulario");
+    this.eventos();
+  }
+}
