26862b2f07b586995d4ebc30228d60386424b72a
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-01-01 14:19:49 -0300

    Add files via upload

---
+++ CLASSES/sec7_class/ejercicio_libro/libroPrototype.js
@@ -0,0 +1,57 @@
+function IngresaLibros() {
+  this.formulario = document.querySelector(".formulario");
+  this.eventos();
+}
+
+IngresaLibros.prototype.eventos = function () {
+  this.formulario.addEventListener("submit", (ev) => {
+    this.handleSubmit(ev);
+    // ev.preventDefault();
+    // this.validaCampos();
+  });
+};
+
+IngresaLibros.prototype.handleSubmit = function (ev) {
+  ev.preventDefault();
+  if (this.validaCampos()) {
+  }
+};
+
+// validación que los campos hayan sido llenados
+IngresaLibros.prototype.validaCampos = function () {
+  let validacion = true;
+  for (let textoError of this.formulario.querySelectorAll(".error-text")) {
+    textoError.remove();
+  }
+
+  this.formulario.querySelectorAll(".validar").forEach((campo) => {
+    if (!campo.value) {
+      this.mensajeError(campo, "Debe llenar campo");
+      validacion = false;
+    }
+
+    if (campo.classList.contains("year")) {
+      if (
+        isNaN(campo.value) ||
+        campo.value.length !== 4 ||
+        campo.value < 1850 ||
+        campo.value > 2023
+      ) {
+        this.mensajeError(campo, "Debes ingresar un año válido");
+        validacion = false;
+      }
+    }
+  });
+  return validacion;
+};
+
+IngresaLibros.prototype.mensajeError = function (campo, mensaje) {
+  let div = document.createElement("div");
+  div.innerHTML = mensaje;
+  div.style.color = "blue";
+  div.style.fontSize = "15px";
+  div.classList.add("error-text");
+  campo.insertAdjacentElement("afterend", div);
+};
+
+const libro = new IngresaLibros();
