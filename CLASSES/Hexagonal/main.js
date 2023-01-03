df6352e4af41ce004b38c7a16d7683cd09dfe559
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-01-03 09:23:12 -0300

    Add files via upload

--- CLASSES/Hexagonal/main.js
+++ CLASSES/Hexagonal/main.js
@@ -4,21 +4,33 @@ class Hexagonal {
     this.events();
   }
 
+  // administra eventos
   events() {
     this.imagenes.forEach((imagen) => {
       imagen.addEventListener("mouseover", (ev) => {
         this.handleOver(ev, imagen);
       });
     });
+
+    this.imagenes.forEach((imagen) => {
+      imagen.addEventListener("mouseout", (ev) => {
+        this.borraMensajes();
+      });
+    });
   }
 
+  // funciones mouseover
   handleOver(ev, imagen) {
     ev.preventDefault();
     this.creaMensaje(ev, imagen);
-    console.log(ev.toElement.classList[1]);
   }
 
-  creaMensaje(ev, imagen) {
+  // función mouseout borra mensaje
+  borraMensajes() {
+    document.querySelector(".mensaje").remove();
+  }
+
+  creaMensaje(ev) {
     let div = document.createElement("div");
     div.innerHTML = `este es ${ev.toElement.classList[1]}`;
     div.style.fontSize = "20px";
