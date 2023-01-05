8140d80ecd2bdaaeb76c5bb3fe3beddffbb65d91
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-01-05 10:58:32 -0300

    Add files via upload

--- CLASSES/sec8_fetch/promises.js
+++ CLASSES/sec8_fetch/promises.js
@@ -4,25 +4,31 @@ function rand(min, max) {
 
 function esperaAi(mensaje, tempo) {
   return new Promise((resolve, reject) => {
+    if (typeof mensaje !== "string") reject("bad value");
     setTimeout(() => {
       resolve(mensaje);
     }, tempo);
   });
 }
 
-console.log("activo");
-esperaAi("mensaje 1", rand(4000, 5000))
+console.log("Mensaje inicial: ");
+esperaAi("mensaje 1: conexão com BD", rand(4000, 5000))
   .then((resposta) => {
     console.log(resposta);
-    return esperaAi("mensaje 2", rand(2000, 3000));
+    return esperaAi(2222, rand(2000, 3000));
   })
   .then((resposta) => {
     console.log(resposta);
-    return esperaAi("mensaje 3", rand(1000, 2000));
+    return esperaAi("mensaje 3: tratando os dados", rand(1000, 2000));
   })
   .then((resposta) => {
     console.log(resposta);
   })
-  .catch();
+  .then(() => {
+    console.log("exibe dados na tela");
+  })
+  .catch((e) => {
+    console.log("Error: ", e);
+  });
 // esperaAi("mensaje 2", rand(2000, 5000));
 // esperaAi("mensaje 3", rand(2000, 5000));
