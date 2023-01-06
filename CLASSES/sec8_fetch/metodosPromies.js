8d7de9cce9a358fc7db6f7dfbcee4ac6a3c00e9a
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-01-06 20:47:03 -0300

    Add files via upload

--- CLASSES/sec8_fetch/metodosPromies.js
+++ CLASSES/sec8_fetch/metodosPromies.js
@@ -4,11 +4,11 @@ function rand(min, max) {
 
 function esperaAi(mensaje, tempo) {
   return new Promise((resolve, reject) => {
-    if (typeof mensaje !== "string") {
-      reject(false);
-      return;
-    }
     setTimeout(() => {
+      if (typeof mensaje !== "string") {
+        reject("Cai no erro");
+        return;
+      }
       resolve(mensaje);
       return;
     }, tempo);
@@ -18,20 +18,45 @@ function esperaAi(mensaje, tempo) {
 // Estos son los métodos
 // Promise.all Promise.race Promise.resolve Promise.reject
 
-const promises = [
-  "primeiro valor",
-  esperaAi("Promise 1", 3000),
-  esperaAi("Promise 2", 500),
-  esperaAi("Promise 3", 1000),
-  // esperaAi(1000, 1000),
-  "outro valor",
-];
+// const promises = [
+//   // "primeiro valor",
+//   esperaAi("Promise 1", rand(1000, 5000)),
+//   esperaAi("Promise 2", rand(1000, 5000)),
+//   esperaAi("Promise 3", rand(1000, 5000)),
+//   esperaAi(1000, rand(1000, 5000)),
+//   // "outro valor",
+// ];
 
-// sirve para realizar una cadena de promises.
-Promise.all(promises)
-  .then(function (valor) {
-    console.log(valor);
+// Promise.all: sirve para realizar una cadena de promises.
+// Promise.all(promises)
+//   .then(function (valor) {
+//     console.log(valor);
+//   })
+//   .catch(function (erro) {
+//     console.log(erro);
+//   });
+
+// Promie.race: entrega la promesa que se resuelva más rápido
+// Promise.race(promises)
+//   .then(function (valor) {
+//     console.log(valor);
+//   })
+//   .catch(function (erro) {
+//     console.log(erro);
+//   });
+
+function baixaPagina() {
+  const emCache = false;
+
+  if (emCache) {
+    return Promise.resolve("Página em cache");
+  } else {
+    return esperaAi("baixei página", 3000);
+  }
+}
+
+baixaPagina()
+  .then((dadosPagina) => {
+    console.log(dadosPagina);
   })
-  .catch(function (erro) {
-    console.log(erro);
-  });
+  .catch((e) => console.log("Erro", e));
