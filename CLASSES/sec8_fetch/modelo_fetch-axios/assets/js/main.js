c4875c2da905a6103625501c63b91a4798622f47
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-01-09 21:12:00 -0300

    Add files via upload

--- CLASSES/sec8_fetch/modelo_fetch-axios/assets/js/main.js
+++ CLASSES/sec8_fetch/modelo_fetch-axios/assets/js/main.js
@@ -1,1 +1,26 @@
+fetch("pessoas.json")
+  .then((resposta) => resposta.json())
+  .then((pessoas) => carregaDados(pessoas));
 
+function carregaDados(pessoas) {
+  const table = document.createElement("table");
+  const resultados = document.querySelector(".resultados");
+  for (let pessoa of pessoas) {
+    const tr = document.createElement("tr");
+
+    let td1 = document.createElement("td");
+    td1.innerHTML = pessoa.nome;
+    tr.appendChild(td1);
+
+    let td2 = document.createElement("td");
+    td2.innerHTML = pessoa.idade;
+    tr.appendChild(td2);
+
+    let td3 = document.createElement("td");
+    td3.innerHTML = pessoa.cpf;
+    tr.appendChild(td3);
+
+    table.appendChild(tr);
+  }
+  resultados.appendChild(table);
+}
