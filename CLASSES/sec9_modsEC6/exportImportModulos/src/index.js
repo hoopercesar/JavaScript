fb7206c05368317e54c5de15c5959c2f70e62921
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-01-14 11:13:32 -0300

    Add files via upload

--- CLASSES/sec9_modsEC6/exportImportModulos/src/index.js
+++ CLASSES/sec9_modsEC6/exportImportModulos/src/index.js
@@ -1,6 +1,4 @@
-import { nome, sobrenome, idade, suma } from "./modulo1";
+// import { nome as nome2, sobrenome, idade2, suma } from "./modulo1";
+import * as meuModulo from "./modulo1";
 
-const nome2 = "João";
-
-console.log(nome, nome2, sobrenome, idade);
-console.log(suma(3, 6));
+console.log(meuModulo.suma(2, 5));
