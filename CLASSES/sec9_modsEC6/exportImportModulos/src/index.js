d02299514a039d3fd330f568986f423d59443346
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-01-14 08:19:50 -0300

    Add files via upload

---
+++ CLASSES/sec9_modsEC6/exportImportModulos/src/index.js
@@ -0,0 +1,6 @@
+import { nome, sobrenome, idade, suma } from "./modulo1";
+
+const nome2 = "João";
+
+console.log(nome, nome2, sobrenome, idade);
+console.log(suma(3, 6));
---
+++ CLASSES/sec9_modsEC6/exportImportModulos/src/modulo1.js
@@ -0,0 +1,9 @@
+const nome = "pepe";
+const sobrenome = "soto";
+const idade = 30;
+
+function suma(x, y) {
+  return x + y;
+}
+
+export { nome, sobrenome, idade, suma };
