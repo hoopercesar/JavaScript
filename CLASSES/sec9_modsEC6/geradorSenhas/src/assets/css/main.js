81a213ccc9451ebf644c8e33aab2a12cf8b5cf4c
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-01-16 22:01:17 -0300

    Add files via upload

---
+++ CLASSES/sec9_modsEC6/geradorSenhas/src/assets/css/main.js
@@ -0,0 +1,6 @@
+import "./assets/css/style.css";
+
+const senhaCriada = document.querySelector(".senha-gerada");
+senhaCriada.innerHTML = "Estamos trabalhando porra!!";
+
+const inputs = document.querySelectorAll("input");
