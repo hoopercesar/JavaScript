243ac0593327cf29cf9300805b2419e5f114bfcf
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-01-18 17:44:52 -0300

    Add files via upload

--- CLASSES/sec10_nodejs/mod1.js
+++ CLASSES/sec10_nodejs/mod1.js
@@ -8,9 +8,21 @@ const falaNome = () => {
 
 // formas de exportar con nodejs
 // usando module.exports
-module.exports.nome = nome;
-module.exports.sobrenome = sobrenome;
-module.exports.idade = idade;
-module.exports.funcion = falaNome;
+// module.exports.nome = nome;
+// module.exports.sobrenome = sobrenome;
+// module.exports.idade = idade;
+// module.exports.funcion = falaNome;
+
+// usando exports
+// exports.nombre = ["pepe", "lalo"];
+// exports.apellido = sobrenome;
+// exports.edad = idade;
+// exports.function = falaNome;
+
+// usando this
+this.Name = nome;
+this.lastName = sobrenome;
+this.age = idade;
+this.function = falaNome;
 
 console.log(module);
