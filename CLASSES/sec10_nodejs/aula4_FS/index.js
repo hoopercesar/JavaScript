b75942336d57de9f1d674645c66ee0aacb02ed55
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-01-19 21:05:18 -0300

    Add files via upload

--- CLASSES/sec10_nodejs/aula4_FS/index.js
+++ CLASSES/sec10_nodejs/aula4_FS/index.js
@@ -21,14 +21,27 @@ async function walk(files, rootDir) {
     const fileFullPath = path.resolve(rootDir, file);
     const stats = await fs.stat(fileFullPath);
 
+    // ignora aquellos archivo dentro de node_module
+    // la expresión regular /.nombre-archivo/g.test(camino) busca dentro del path
+    // de cada archivo. Si dentro de ese path existe un trozo donde aparezca
+    // nombre-arhivo, entonces, ignora ese camino (pues se le ordena continuar)
     if (/.node_modules/g.test(fileFullPath)) continue;
 
     if (stats.isDirectory()) {
       readdir(fileFullPath);
       continue;
     }
-    if (!/\.css$/g.test(fileFullPath)) continue;
+
+    // estas expresiones son similares. buscan los caminos donde hay una sec y la ignoran
     if (/.sec/g.test(fileFullPath)) continue;
+
+    // pero este comando es ligeramente diferete. Busca los paths que terminan con css
+    // Si la expresión (path) no termina con css se ignora. Se incluyen también las expresiones
+    // que terminan con html
+    //
+    if (!/\.css$/g.test(fileFullPath) && !/\.html$/g.test(fileFullPath))
+      continue;
+
     console.log(fileFullPath);
   }
 }
