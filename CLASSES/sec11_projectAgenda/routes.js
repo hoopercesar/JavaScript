aed1627967febcd646879b0dc014f982f04c4d42
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-02-03 06:34:43 -0300

    Add files via upload

--- CLASSES/sec11_projectAgenda/routes.js
+++ CLASSES/sec11_projectAgenda/routes.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const route = express.Router();
 const homeController = require("./src/controllers/homeController");
-const contatoController = require("./src/controllers/contatoController");
+const loginController = require("./src/controllers/loginController");
 
 // ruta del home
-route.get("/", homeController.paginaInicial);
+route.get("/", homeController.index);
+
+// login's routes
+route.get("/login/index", loginController.index);
+route.post("/login/register", loginController.register);
 
 module.exports = route;
