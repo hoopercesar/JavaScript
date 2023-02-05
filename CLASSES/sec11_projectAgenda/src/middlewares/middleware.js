53d31132360898285d412e225588011db6864c02
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-02-05 19:11:51 -0300

    Add files via upload

--- CLASSES/sec11_projectAgenda/src/middlewares/middleware.js
+++ CLASSES/sec11_projectAgenda/src/middlewares/middleware.js
@@ -1,5 +1,6 @@
 exports.middlewareGlobal = (req, res, next) => {
-  res.locals.unaVariableLocal = "Este es el valor de la variable Local";
+  res.locals.errors = req.flash("errors");
+  res.locals.success = req.flash("success");
   next();
 };
 
