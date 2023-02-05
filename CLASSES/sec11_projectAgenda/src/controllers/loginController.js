57445bed7d9b90e255cd9f32b52ac2074ccbae5c
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-02-05 19:11:19 -0300

    Add files via upload

--- CLASSES/sec11_projectAgenda/src/controllers/loginController.js
+++ CLASSES/sec11_projectAgenda/src/controllers/loginController.js
@@ -4,9 +4,29 @@ exports.index = (req, res) => {
   res.render("login");
 };
 
-exports.register = (req, res) => {
-  const login = new Login(req.body);
-  login.register();
-  // it posts in page the user information sended
-  res.send(login.errors);
+exports.register = async function (req, res) {
+  try {
+    const login = new Login(req.body);
+    await login.register();
+
+    // checking errors and create errors messagens
+    if (login.errors.length > 0) {
+      req.flash("errors", login.errors);
+      req.session.save(function () {
+        return res.redirect("/login/index");
+      });
+      return;
+    }
+
+    // it posts in page the user information sended
+    // return res.send(login.errors);
+
+    req.flash("Success", "User was created succefully");
+    req.session.save(function () {
+      return res.redirect("/login/index");
+    });
+  } catch (err) {
+    console.log(err);
+    return res.render("404");
+  }
 };
