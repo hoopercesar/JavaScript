11cc0f93df10e4b7d1c78e8cac97001d858c15d7
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-02-06 09:54:02 -0300

    Add files via upload

--- CLASSES/sec11_projectAgenda/src/controllers/loginController.js
+++ CLASSES/sec11_projectAgenda/src/controllers/loginController.js
@@ -1,7 +1,33 @@
 const Login = require("../models/LoginModel");
 
+exports.login = async function (req, res) {
+  try {
+    const login = new Login(req.body);
+    await login.login();
+
+    // checking errors and creating errors messagens
+    if (login.errors.length > 0) {
+      req.flash("errors", login.errors);
+      req.session.save(function () {
+        return res.redirect("/login/index");
+      });
+      return;
+    }
+
+    req.flash("Success", "You were logged succefully");
+    req.session.user = login.user;
+    req.session.save(function () {
+      return res.redirect("/login/index");
+    });
+  } catch (err) {
+    console.log(err);
+    return res.render("404");
+  }
+};
+
 exports.index = (req, res) => {
-  res.render("login");
+  if (req.session.user) return res.render("login-logado");
+  return res.render("login");
 };
 
 exports.register = async function (req, res) {
@@ -29,4 +55,9 @@ exports.register = async function (req, res) {
     console.log(err);
     return res.render("404");
   }
+};
+
+exports.logout = function (req, res) {
+  req.session.destroy();
+  res.redirect("/");
 };
