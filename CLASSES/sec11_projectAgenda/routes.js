c6aab027646e01940f03c8059d24443560bfa894
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-02-06 22:35:08 -0300

    Add files via upload

--- CLASSES/sec11_projectAgenda/routes.js
+++ CLASSES/sec11_projectAgenda/routes.js
@@ -2,6 +2,9 @@ const express = require("express");
 const route = express.Router();
 const homeController = require("./src/controllers/homeController");
 const loginController = require("./src/controllers/loginController");
+const contactController = require("./src/controllers/contactController");
+
+const { loginRequired } = require("./src/middlewares/middleware");
 
 // ruta del home
 route.get("/", homeController.index);
@@ -9,5 +12,11 @@ route.get("/", homeController.index);
 // login's routes
 route.get("/login/index", loginController.index);
 route.post("/login/register", loginController.register);
+route.post("/login/login", loginController.login);
+route.get("/login/logout", loginController.logout);
+
+// routes for creating contacts. middeware in the middle of the route
+// for users not conected. they must be logged in
+route.get("/contact/index", loginRequired, contactController.index);
 
 module.exports = route;
