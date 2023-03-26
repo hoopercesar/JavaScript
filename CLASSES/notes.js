bc13843d4335633d6b44aa8fdc52bf57e024426f
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-03-25 22:24:27 -0300

    Add files via upload

---
+++ CLASSES/notes.js
@@ -0,0 +1,102 @@
+const express = require("express");
+const router = express.Router();
+const Note = require("../models/Note");
+
+// database
+
+const { MongoClient } = require("mongodb");
+const uri = require("../atlas_uri");
+
+const client = new MongoClient(uri);
+const dbname = "Chat";
+const collection_name = "conversaciones";
+
+const accountsCollection = client.db(dbname).collection(collection_name);
+
+const connectToDatabase = async () => {
+  try {
+    await client.connect();
+    console.log(`Te has conectado al ${dbname} con éxito`);
+
+    // Lista las bases de datos en el cluster
+    const dbs = await client.db().admin().listDatabases();
+    // console.table(dbs.databases);
+  } catch (err) {
+    console.error(`Error de conección a la DB, ${err}`);
+  }
+};
+
+router.get("/notes/add", (req, res) => {
+  res.render("notes/new-note");
+});
+
+router.post("/notes/new-note", async (req, res) => {
+  const { title, description } = req.body;
+  const errors = [];
+
+  if (!title) errors.push({ text: "Write a Title" });
+  if (!description) errors.push({ text: "Write a description" });
+
+  if (errors.length > 0) {
+    res.render("notes/new-note", {
+      errors,
+      title,
+      description,
+    });
+  } else {
+    const note = new Note({ title, description });
+    const sampleAccounts = [
+      {
+        speak: 2,
+        name: note.title,
+        mensaje: note.description,
+        hora: note.date,
+      },
+    ];
+    const documentsToFind = { speak: 2 };
+    let chat = [];
+
+    const main = async () => {
+      try {
+        await connectToDatabase();
+
+        let datos = await accountsCollection.insertMany(sampleAccounts);
+        let mensajes = await accountsCollection.find(documentsToFind).toArray();
+        mensajes.map((mensaje) => {
+          chat.push({
+            texto: `${mensaje.name.toUpperCase()}: ${mensaje.mensaje}`,
+          });
+        });
+        res.render("notes/new-note", {
+          chat,
+        });
+        console.log(chat.length);
+        // chat = `${mensajes.mensaje}`
+
+        // mensajes.map((mensaje) => {
+        //   console.log(mensaje);
+        // });
+      } catch (e) {
+        console.error(`Error de conexión, ${e}`);
+      } finally {
+        await client.close();
+      }
+    };
+    main();
+
+    // const chat = [];
+    // let hora = `${note.date.getHours()}:${note.date.getMinutes()}:${note.date.getSeconds()}`;
+    // chat.push(`${note.title.toUpperCase()}: (${hora}) - ${note.description}`);
+
+    // res.render("notes/new-note", {
+    //   chat,
+    // });
+    // console.log("en notes", note);
+  }
+});
+
+router.get("notes", (req, res) => {
+  res.send("notes from database");
+});
+
+module.exports = router;
